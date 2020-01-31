a4c3a9f92baf9a3b304f12d72ae320a684c2471e
Chris <clee86@gmail.com>
2020-01-30 18:10:13 -0800

    Delete unsplash.js

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com/",
-  headers: {
-    Authorization:
-      "Client-ID 9bd92242479cd2fae0f197e1b6ef8fda23f382ef8b2371f580ffef42fffa156a"
-  }
-});
